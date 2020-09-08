9d23939d854a8a4ea5f899ee9ee968adb40ccf6a
Sabry Razick <sabryr@gmail.com>
2020-09-08 08:55:31 +0200

    remove custom blocks

--- setdir.sh
+++ setdir.sh
@@ -43,7 +43,6 @@ echo "   --repositorypath=$repositorypath  \ "  >> SETUP.local
 echo "   --tmp-logdir=$tmp_logdir \ "  >> SETUP.local
 echo "   --sourcepath=$source_path \ "  >> SETUP.local
 echo "   --tmpdir=$tmpdir  \ "  >> SETUP.local
-echo "   --include-easyblocks=$include_easyblocks \ "  >> SETUP.local
 echo "   --robot $(pwd): <EASYBUILD-FILE> "  >> SETUP.local
 
 echo "Local setup saved in SETUP.local"
